1bdaa78aa9e72dfd44d4f1952dca9bc949bf39d0
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-12 17:12:45 +0300

    Assignment 2

---
+++ calculateAge.js
@@ -0,0 +1,5 @@
+// calculate age using current year and and year of birth assigned to variable date
+var date = 1995;
+var currentYear = 2019;
+ var age = currentYear - date ;
+ console.log(age + "years");
