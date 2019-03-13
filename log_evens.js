14081d992433b51afebccf7f9dc6381151b8eda0
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-13 11:27:15 +0300

    changed a comment

--- log_evens.js
+++ log_evens.js
@@ -1,4 +1,4 @@
-// loops through numbers between 0 -20 and outputs even numbers
+// loops through numbers between 2 -20 and outputs even numbers
 
 for(var even = 2; even <= 20; even++){
   if(even%2 ===0){
