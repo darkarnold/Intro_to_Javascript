10c8215774bb5da64eb011c88718ca20ff5dcefc
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-13 13:04:52 +0300

    greater than 100 program

---
+++ try_100.js
@@ -0,0 +1,12 @@
+// Enter a number greater than 100
+// check if it greater than 100
+//output greater if it is greater than 100
+// output try again if it is less than
+
+let number = 56;
+if (number > 100) {
+  console.log("Greater");
+
+}else {
+  console.log("try again")
+};
