d6a4407a1457759bd9e086268280f33c8629c930
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-14 14:41:11 +0300

    changed prime_logger to function

--- functions/prime_logger.js
+++ functions/prime_logger.js
@@ -1,14 +1,23 @@
+// Function takes in a value
 // takes in a value of n and returns prime numbers within the range of n
-var n=100;
-for (var num = 2; num <= n; num++) {
 
-  var notPrime = false;
-  for (var i = 2; i <= num; i++) {
-      if (num%i===0 && i!==num) {
-          notPrime = true;
-      }
-  }
-  if (notPrime === false) {
+//var n=100;
+
+
+const number = (n) =>
+{
+  for (var num = 2; num <= n; num++) {
+
+    var notPrime = false;
+    for (var i = 2; i <= num; i++) {
+        if (num%i===0 && i!==num) {
+            notPrime = true;
+        }
+    }
+    if (notPrime === false) {
     console.log(num);
+
   }
-}
+    }
+  }
+  console.log(number(100));
