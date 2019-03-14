9149a02b767ece03a2e82908296d058df5f8244f
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-14 14:15:36 +0300

    changed billing to function

--- functions/billing.js
+++ functions/billing.js
@@ -1,9 +1,19 @@
+
+//Function to calculate billing
 // This is the percentage that determines the transaction charge
-const transactionCharge = 0.025;
+// transanctionCharge is a percentage
+ const billing = (amountTendered ,transactionCharge) =>{
 
-let amountTendered = 100000;
+  //const transactionCharge = 0.025;
 
-console.log("This is what the billed amount is (less transaction charges);");
+  //let amountTendered = 100000;
 
-//Print out the result
-console.log(amountTendered - transactionCharge*amountTendered);
+  console.log("This is what the billed amount is (less transaction charges);");
+
+  //Print out the result
+    return amountTendered - transactionCharge*amountTendered;
+
+
+}
+
+ console.log(billing(100000,0.025));
