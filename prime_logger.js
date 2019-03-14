a39b773aef7d9751beb5f952b1894025ab8560ad
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-14 10:00:20 +0300

    Prime logger works

--- prime_logger.js
+++ prime_logger.js
@@ -1,14 +1,14 @@
 // takes in a value of n and returns prime numbers within the range of n
+var n=100;
+for (var num = 2; num <= n; num++) {
 
-let z = 20;
-
-nextPrime:
-for(let x = 2; x <= z; x++){
-
-  for( let y =2; y < x; y++){
-    if (x % y === 0) continue; nextPrime;
-
-
+  var notPrime = false;
+  for (var i = 2; i <= num; i++) {
+      if (num%i===0 && i!==num) {
+          notPrime = true;
+      }
   }
-  console.log(x);
+  if (notPrime === false) {
+    console.log(num);
+  }
 }
